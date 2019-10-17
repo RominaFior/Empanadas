2f9f1962c0a88baba7d08e073d894597b4cefcad
Romina Fior <fior.romina4@gmail.com>
2019-10-18 01:17:43 +0200

    componente about y respectiva ruta en navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,10 @@ import { APP_ROUTING } from "./routing.routes";
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./shared/navbar/navbar.component";
 import { InicioComponent } from "./components/inicio/inicio.component";
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, InicioComponent],
+  declarations: [AppComponent, NavbarComponent, InicioComponent, AboutComponent],
   imports: [BrowserModule, APP_ROUTING],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/routing.routes.ts
+++ src/app/routing.routes.ts
@@ -2,15 +2,15 @@ import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
 
 import { InicioComponent } from "./components/inicio/inicio.component";
-/* import { Name2Component } from './';
-import { Name3Component } from './';
+import { AboutComponent } from "./components/about/about.component";
+/*import { Name2Component } from './';
 import { Name4Component } from './';
 import { PageNotFoundComponent } from './'; */
 
 const APP_ROUTES: Routes = [
   { path: "home", component: InicioComponent },
+  { path: "about", component: AboutComponent },
   /*  { path: 'path2', component: Name2Component },
-  { path: 'path3', component: Name3Component },
   { path: 'path4', component: Name4Component }, */
   { path: "**", pathMatch: "full", redirectTo: "home" }
 ];
